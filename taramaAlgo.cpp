2f39fd4bde5118dfe058a5f7530da6cc83bedc3a
Dhairya <dhairya.ostwal@gmail.com>
2020-08-28 22:49:25 +0530

    Updated schedule()

--- taramaAlgo.cpp
+++ taramaAlgo.cpp
@@ -58,18 +58,28 @@ void inputCheck()
     cout << "\nPlease wait patiently we shall optimise your waiting time" << endl;
 }
 
-// void scheduleCheck()
-// {
-//     // cout << "\nTiming Schedule:\n1. 30 - 5 = 25\n2. 25 - 10 = 15\n3. 15 - 2 = 13\n4. 13 - 5 = 8\n5. 8 - 8 = 0\n";
-
-//     for (int i = 0; i < 5; i++)
-//     {
-//         if (a[i] == 5)
-//         {
-//             cout << "Waiting Time: " << 5 << " units";
-//         }
-//     }
-// }
+void scheduleCheck()
+{
+    // cout << "\nTiming Schedule:\n1. 30 - 5 = 25\n2. 25 - 10 = 15\n3. 15 - 2 = 13\n4. 13 - 5 = 8\n5. 8 - 8 = 0\n";
+    int capacity = 30;
+    for (int i = 0; i < 5; i++)
+    {
+        int vacant = fullyEmpty[i];
+        cout << "Vacancy = " << vacant << endl;
+        capacity -= fullyEmpty[i];
+        cout << "Capacity = " << capacity << endl
+             << endl;
+        for (int i = 0; i < 5; i++)
+        {
+            if (waitingLine[i] <= vacant && waitingLine[i] > 0)
+            {
+                capacity += waitingLine[i];
+                cout << "Inserted = " << waitingLine[i] << endl;
+                waitingLine[i] = -1;
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -81,15 +91,7 @@ int main()
     cout << "\nWelcome to Tarama!\nLet's start the waiting line" << endl;
     inputCheck();
 
-    cout << "\n--- Already occupant will exit in asc. order ---\n"
-         << endl;
     sort(fullyEmpty, fullyEmpty + 5);
-    for (int i = 0; i < 5; i++)
-    {
-        cout << fullyEmpty[i] << " ";
-    }
-    cout << endl;
-
-    // scheduleCheck();
+    scheduleCheck();
     return 0;
 }
