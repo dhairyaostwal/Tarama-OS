21cd645c77af6ced669b9961de46f4c9e064aa43
Dhairya <dhairya.ostwal@gmail.com>
2020-10-26 07:32:44 +0530

    Removed Scale in pdf

--- report.py
+++ report.py
@@ -1,21 +1,21 @@
 from app import result, display
 print(result)
-def drawMyRuler(pdf):
-    pdf.drawString(100,810, 'x100')
-    pdf.drawString(200,810, 'x200')
-    pdf.drawString(300,810, 'x300')
-    pdf.drawString(400,810, 'x400')
-    pdf.drawString(500,810, 'x500')
-    pdf.drawString(600,810, 'x600')
+# def drawMyRuler(pdf):
+#     pdf.drawString(100,810, 'x100')
+#     pdf.drawString(200,810, 'x200')
+#     pdf.drawString(300,810, 'x300')
+#     pdf.drawString(400,810, 'x400')
+#     pdf.drawString(500,810, 'x500')
+#     pdf.drawString(600,810, 'x600')
 
-    pdf.drawString(10,100, 'y100')
-    pdf.drawString(10,200, 'y200')
-    pdf.drawString(10,300, 'y300')
-    pdf.drawString(10,400, 'y400')
-    pdf.drawString(10,500, 'y500')
-    pdf.drawString(10,600, 'y600')
-    pdf.drawString(10,700, 'y700')
-    pdf.drawString(10,800, 'y800')
+#     pdf.drawString(10,100, 'y100')
+#     pdf.drawString(10,200, 'y200')
+#     pdf.drawString(10,300, 'y300')
+#     pdf.drawString(10,400, 'y400')
+#     pdf.drawString(10,500, 'y500')
+#     pdf.drawString(10,600, 'y600')
+#     pdf.drawString(10,700, 'y700')
+#     pdf.drawString(10,800, 'y800')
 
 
 from reportlab.pdfgen import canvas
@@ -32,7 +32,6 @@ pdf.setTitle(documentTitle)
 pdf.drawString(270,770,title)
 pdf.drawString(210,750,subtitle)
 
-
 pdf.drawString(200,690,textlines[0])
 pdf.drawString(300,690,result[3])
 
@@ -48,6 +47,6 @@ pdf.drawString(300,615,result[5])
 pdf.drawString(200,590,textlines[4])
 pdf.drawString(300,590,result[6])
 
-drawMyRuler(pdf)
+# drawMyRuler(pdf)
 
 pdf.save()
